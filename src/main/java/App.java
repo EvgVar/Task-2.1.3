db728427094277aba437f756f249617cd29dfcd3
Evgeny Varlamov <varlamov-el@mail.ru>
2023-01-09 23:26:14 +0300

    Cat bean implemented

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanAdditional = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("HelloWorld pair: " + (bean == beanAdditional));
+
+        Cat catFirst = (Cat) applicationContext.getBean("cat");
+        Cat catSecond = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Cat pair: " + (catFirst == catSecond));
     }
 }
